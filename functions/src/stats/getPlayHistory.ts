c84e16c884b1f033798d82cfd0b88e4ab113b182
Wing-Hou Chan <winghouchan@gmail.com>
2021-10-07 16:46:31 +0100

    Add ability to return intervals of data

--- functions/src/stats/getPlayHistory.ts
+++ functions/src/stats/getPlayHistory.ts
@@ -2,25 +2,15 @@ import admin from "firebase-admin";
 import { logger } from "firebase-functions";
 import { UserPlayHistoryObject } from "../playHistory";
 
-/**
- * The smallest possible time value in JavaScript. Used for defining the
- * beginning of the time boundary to search for records in.
- * @see https://262.ecma-international.org/5.1/#sec-15.9.1.1
- */
-const minDate = -8640000000000000;
-
 export default async function getPlayHistory({
-  after,
-  before,
+  start,
+  end,
   userId,
 }: {
-  after?: number;
-  before?: number;
+  start: number;
+  end: number;
   userId: string;
 }) {
-  const startAt = `${typeof after === "number" ? after : minDate}`;
-  const endAt = `${typeof before === "number" ? before : Date.now()}`;
-
   logger.log("Reading user's play history");
 
   const playHistory: UserPlayHistoryObject["history"] = (
@@ -28,8 +18,8 @@ export default async function getPlayHistory({
       .database()
       .ref(`playHistory/${userId}/history`)
       .orderByKey()
-      .startAt(startAt)
-      .endAt(endAt)
+      .startAt(`${start}`)
+      .endAt(`${end}`)
       .once("value")
   ).val();
 
--- functions/src/stats/index.ts
+++ functions/src/stats/index.ts
@@ -1,21 +1,68 @@
+import {
+  eachHourOfInterval,
+  eachDayOfInterval,
+  eachWeekOfInterval,
+  eachMonthOfInterval,
+  endOfHour,
+  endOfDay,
+  endOfWeek,
+  endOfMonth,
+  isWithinInterval,
+  minTime,
+} from "date-fns";
 import { https, logger } from "firebase-functions";
 import { mean, median, mode } from "mathjs";
 import { PlayHistoryWithAudioFeatures } from "../playHistory";
 import getPlayHistory from "./getPlayHistory";
 
 interface RequestData {
-  after?: number;
-  before?: number;
+  start?: number;
+  end?: number;
+  interval?: "hour" | "day" | "week" | "month";
 }
 
+/**
+ * Signifies weeks start on Monday
+ * @see https://date-fns.org/v2.25.0/docs/Day
+ */
+const weekStartsOn = 1;
+
+const startIntervalFn = {
+  hour: eachHourOfInterval,
+  day: eachDayOfInterval,
+  week: (interval: Interval) => eachWeekOfInterval(interval, { weekStartsOn }),
+  month: eachMonthOfInterval,
+};
+
+const endIntervalFn = {
+  hour: endOfHour,
+  day: endOfDay,
+  week: (date: number | Date) => endOfWeek(date, { weekStartsOn }),
+  month: endOfMonth,
+};
+
+/**
+ * Get valence statistics from play history. Valence is a measure positivity. 0
+ * valence is negative while 1 valence is positive.
+ *
+ * @param {RequestData} [data]
+ * @param {number} [data.start] Get statistics for data starting from this date value (inclusive)
+ * @param {number} [data.end] Get statistics for data ending at this date value (inclusive)
+ * @param {('hour'|'day'|'week'|'month')} [data.interval] Group data into the specified interval
+ *
+ * @see `valence` in https://developer.spotify.com/documentation/web-api/reference/#object-audiofeaturesobject
+ */
 const stats = https.onCall(async (data: RequestData | null, context) => {
-  const { after, before } = data ?? {};
+  const { interval: intervalParam } = data ?? {};
   const userId = context.auth?.uid;
+
+  const start = typeof data?.start === "number" ? data.start : minTime;
+  const end = typeof data?.end === "number" ? data.end : Date.now();
 
   if (userId) {
     const playHistory = await getPlayHistory({
-      after,
-      before,
+      start,
+      end,
       userId,
     });
 
@@ -26,7 +73,7 @@ const stats = https.onCall(async (data: RequestData | null, context) => {
         .map(({ audio_features }) => audio_features?.valence)
         .filter((valence): valence is number => valence !== undefined);
 
-      const stats = {
+      const periodStats = {
         max: Math.max(...valence),
         mean: mean(valence),
         median: median(valence),
@@ -34,15 +81,52 @@ const stats = https.onCall(async (data: RequestData | null, context) => {
         mode: mode(valence),
       };
 
+      if (intervalParam) {
+        const timestamps = Object.keys(playHistory);
+
+        const intervalStats = startIntervalFn[intervalParam]({
+          start: Number(timestamps[0]),
+          end: Number(timestamps[timestamps.length - 1]),
+        }).map((interval) => {
+          const valenceInInterval = Object.entries(playHistory)
+            .filter(([key]) =>
+              isWithinInterval(Number(key), {
+                start: interval,
+                end: endIntervalFn[intervalParam](interval),
+              })
+            )
+            .map(([, { audio_features }]) => audio_features?.valence)
+            .filter((valence): valence is number => valence !== undefined);
+
+          return {
+            interval: Number(interval),
+            max: valenceInInterval.length
+              ? Math.max(...valenceInInterval)
+              : null,
+            mean: valenceInInterval.length ? mean(valenceInInterval) : null,
+            median: valenceInInterval.length ? median(valenceInInterval) : null,
+            min: valenceInInterval.length
+              ? Math.min(...valenceInInterval)
+              : null,
+            mode: valenceInInterval.length ? mode(valenceInInterval) : null,
+          };
+        });
+
+        return {
+          period: periodStats,
+          interval: intervalStats,
+        };
+      }
+
       logger.log("Successfully calculated statistics");
 
-      return stats;
+      return { period: periodStats };
     }
 
     logger.log("No play history found");
   }
 
-  return {};
+  return { period: {}, interval: [] };
 });
 
 export default stats;
